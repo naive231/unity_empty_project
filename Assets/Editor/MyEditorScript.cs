90abbae78f07fcde09ffabcc14dfcb0936dc6817
naivechou <naive231@gmail.com>
2024-09-11 21:06:01 +0800

    Add '.vs/' to ignoring list. Move 'MyEditorScript.cs' to 'Assets/Editor/'.

---
+++ Assets/Editor/MyEditorScript.cs
@@ -0,0 +1,16 @@
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+class MyEditorScript
+{
+    static void PerformBuild ()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/SampleScene.unity" };
+        buildPlayerOptions.locationPathName = "Builds/MyAndroidBuild.apk"; 
+        buildPlayerOptions.target = BuildTarget.Android; 
+        buildPlayerOptions.options = BuildOptions.None; 
+
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+} 
